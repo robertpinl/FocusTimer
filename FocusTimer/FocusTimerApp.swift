7eb71dbd7d1c0ad8d66aca5af1d537bf238a5212
Robert Pinl <robert.pinl@icloud.com>
2023-02-25 11:39:01 +1300

    Initial Commit

---
+++ FocusTimer/FocusTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimerApp.swift
+//  FocusTimer
+//
+//  Created by Robert P on 25.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

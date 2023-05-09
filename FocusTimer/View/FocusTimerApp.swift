39bb09606a19997c930aa2db0a12fcfb8bb86512
Robert Pinl <robert.pinl@icloud.com>
2023-05-09 18:32:25 +1200

    Update app id

--- FocusTimer/View/FocusTimerApp.swift
+++ FocusTimer/View/FocusTimerApp.swift
@@ -14,7 +14,7 @@ struct FocusTimerApp: App {
     @StateObject var persistenceController = PersistenceController()
     
     init() {
-        let configuration = TelemetryManagerConfiguration(appID: valueForAppId())
+        let configuration = TelemetryManagerConfiguration(appID: AppId.value)
         TelemetryManager.initialize(with: configuration)
         TelemetryManager.send("appLaunchedRegularly")
     }
